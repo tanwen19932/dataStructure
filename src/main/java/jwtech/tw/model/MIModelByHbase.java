d6ba30abd1a1e793bc58b007d2a9c1b6a7eb8d41
tanwen19932 <tanwen19932@163.com>
2016-12-08 16:46:05 +0800

    Spilt MODEL and TRAIN method

---
+++ src/main/java/jwtech/tw/model/MIModelByHbase.java
@@ -0,0 +1,14 @@
+package jwtech.tw.model;
+
+/**
+ * @author TW
+ * @date TW on 2016/12/6.
+ */
+public class MIModelByHbase extends AbstractModel  {
+
+
+    @Override
+    public void handleLine(String line) {
+
+    }
+}
--- src/main/java/jwtech/tw/model/TF_IDFModel.java
+++ src/main/java/jwtech/tw/model/TF_IDFModel.java
@@ -1,14 +1,9 @@
 package jwtech.tw.model;
 
-import com.google.common.io.Files;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.text.DecimalFormat;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -60,55 +55,5 @@ public class TF_IDFModel extends AbstractModel {
         }
     }
 
-    public static void trainFromFile(String filePath, String savePath)
-            throws IOException {
-        CountMap<String> wordDocNumMap = new CountMap<>();
-        CountMap<String> wordTotalNumMap = new CountMap<>();
-        long totalDocSize = 0;
-        long totalWordSize = 0;
-        BufferedReader br = Files.newReader(new File(filePath), Charset.forName("utf-8"));
-        String line;
-        int lineNum = 0;
-        while ((line = br.readLine()) != null) {
-            lineNum++;
-            LOG.info("读取到：第" + lineNum);
-            if (line.trim().length() == 0) {
-                break;
-            }
-            String[] pairs = line.split("\t");
-            for (String pair : pairs) {
-                String key = pair.split(" ")[0];
-                long value = Long.parseLong(pair.split(" ")[1]);
-                totalWordSize += value;
-                wordDocNumMap.add(key);
-                wordTotalNumMap.add(key, value);
-            }
-            totalDocSize++;
-        }
-        br.close();
-        StringBuilder sb = new StringBuilder();
-        DecimalFormat df = new DecimalFormat("#,##0.00000000");//保留8位小数且不用科学计数法
-        for (Map.Entry<String, Long> entry : wordTotalNumMap.entrySet()) {
-            String word = entry.getKey();
-            long wordCount = entry.getValue();
-            long wordDocCount = wordDocNumMap.get(entry.getKey());
-            double tf = jwtech.tw.algo.TF_IDF.tf(wordCount, totalWordSize);
-            double idf = jwtech.tw.algo.TF_IDF.idf(wordDocCount, totalDocSize);
-            double tf_idf = jwtech.tw.algo.TF_IDF.tf_idf(tf, idf);
-            sb.append(word).append("\t")
-                    .append(df.format(tf_idf))
-                    .append("\r\n");
-        }
-        Files.append(sb, new File(savePath), Charset.defaultCharset());
-    }
 
-
-    public static void main(String[] args)
-            throws IOException {
-        System.out.println(TF_IDFModel.getInstance().getWordTF_IDF("ぁと"));
-        //trainFromFile( "/Users/TW/ja_all/all","/Users/TW/ja_all/ja_tfidf" );
-
-        //System.out.println(TF_IDF.class.getResource( "/0" ));
-        //System.out.println(TF_IDF.class.getClassLoader().getResource( "conf/0" ));
-    }
 }
